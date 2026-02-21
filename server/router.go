d4fb1d69407d8e4afa5313c83d19d5239b028be2
Orazgeldiyew <orazgeldiyewrustam26@gmail.com>
2026-02-22 01:27:10 +0500

    :bug: fix: cors

--- server/router.go
+++ server/router.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"strings"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -39,16 +40,12 @@ func NewRouter(deps Deps) *gin.Engine {
 	r.Use(middleware.RequestID())
 
 	// ── CORS ──
-
-	// r.Use(cors.New(cors.Config{
-	// 	AllowOrigins:     *,
-	// 	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-	// 	AllowHeaders:     []string{"Authorization", "Content-Type"},
-	// 	AllowCredentials: true,
-	// 	MaxAge:           12 * time.Hour,
-	// }))
-
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		MaxAge:          12 * time.Hour,
+	}))
 
 	r.Use(ErrorMiddleware())
 
